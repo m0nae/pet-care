e5db4a8ff9cda51536723612e6736414d83314ec
A. Monae Payne <amonaepayne@gmail.com>
2021-05-28 02:42:00 -0500

    :heavy_plus_sign::sparkles: add react router
    Installed react router and set it up.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/">
+            <p>Landing page</p>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
